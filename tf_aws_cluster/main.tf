53fce1b0bc788b34157d979f5385fba0519c3086
alstard <alastair.dickson@gmail.com>
2018-01-24 14:42:03 +0000

    Added AWS scripts and config but not working with Test Kitchen

---
+++ tf_aws_cluster/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_security_group" "atd-ssh-http-allow" {
+  name = "atd-ssh-http-allow"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  security_groups = [
+    "atd-ssh-http-allow"
+  ]
+}
