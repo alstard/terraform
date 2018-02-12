638e6d09d79e1e008df9564fb1cec08251925a72
alstard <alastair.dickson@gmail.com>
2018-02-12 13:34:13 +0000

    Updated lambda functions to deal with ASG

--- lambda_functions/startec2.py
+++ lambda_functions/startec2.py
@@ -3,8 +3,20 @@ import boto3
 region = 'eu-west-2'
 
 def lambda_handler(event, context):
+    # Resume Launch process in any ASG groups Owned by atd
+    asg_client = boto3.client('autoscaling')
+    asg_paginator = asg_client.get_paginator('describe_auto_scaling_groups')
+    asg_page_iterator = asg_paginator.paginate(PaginationConfig={'PageSize': 100})
+    atd_asgs = asg_page_iterator.search('AutoScalingGroups[] | [?contains(Tags[?Key==`{}`].Value, `{}`)]'.format('Owner', 'atd'))
+    ProcessesToResume = ['Launch']
+
+    for asg in atd_asgs:
+        print 'INFO: Resuming Launch Process for ASG: ' + asg['AutoScalingGroupName']
+        asg_client.resume_processes(AutoScalingGroupName=asg['AutoScalingGroupName'], ScalingProcesses=ProcessesToResume)
+
+    # We will explicitly startup all ec2 instances
     ec2resource = boto3.resource('ec2', region_name=region)
-    stopped_atd_instances_filter = [{ 'Name': 'instance-state-name', 'Values': ['stopped']}, {'Name': 'tag:Name', 'Values': ['atd-*']}]
+    stopped_atd_instances_filter = [{ 'Name': 'instance-state-name', 'Values': ['stopped']}, {'Name': 'tag:Owner', 'Values': ['atd']}]
     stopped_instances = ec2resource.instances.filter(Filters=stopped_atd_instances_filter)
 
     ec2iids = []
--- lambda_functions/stopec2.py
+++ lambda_functions/stopec2.py
@@ -1,16 +1,29 @@
 import boto3
+import sys
 
 region = 'eu-west-2'
 
 def lambda_handler(event, context):
-    ec2resource = boto3.resource('ec2', region_name=region)
-    is_mybox_running_filter = [{'Name': 'instance-state-name', 'Values': ['running']}, {'Name': 'tag:Name', 'Values': ['atd-*']}]
-    running_instances = ec2resource.instances.filter(Filters=is_mybox_running_filter)
+    # Suspend Launch process in any ASG groups Owned by atd
+    asg_client = boto3.client('autoscaling')
+    asg_paginator = asg_client.get_paginator('describe_auto_scaling_groups')
+    asg_page_iterator = asg_paginator.paginate(PaginationConfig={'PageSize': 100})
+    atd_asgs = asg_page_iterator.search('AutoScalingGroups[] | [?contains(Tags[?Key==`{}`].Value, `{}`)]'.format('Owner', 'atd'))
+    ProcessesToSuspend = ['Launch']
 
+    for asg in atd_asgs:
+        print 'INFO: Suspending Launch Process for ASG: ' + asg['AutoScalingGroupName']
+        asg_client.suspend_processes(AutoScalingGroupName=asg['AutoScalingGroupName'], ScalingProcesses=ProcessesToSuspend)
+
+    # Get the Running ec2 instance ids
+    ec2resource = boto3.resource('ec2', region_name=region)
+    is_mybox_running_filter = [{'Name': 'instance-state-name', 'Values': ['running']}, {'Name': 'tag:Owner', 'Values': ['atd']}]
+    running_instances = ec2resource.instances.filter(Filters=is_mybox_running_filter)
     ec2ids = []
     for instance in running_instances:
         ec2ids.append(instance.id)
 
+    # Shutdown any instances
     if not ec2ids:
         print 'INFO: You have no instances to stop.'
     else:
